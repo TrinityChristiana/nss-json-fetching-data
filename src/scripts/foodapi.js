29e156f6231a30d269aa0523109a1ea7fbc2bd28
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 15:29:30 -0600

    added fetch for barcode data

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,11 +1,32 @@
 fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food);
-            addFoodToDom(foodAsHTML);
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food); // Should have a `barcode` property
+
+            // Now fetch the food from the Food API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text) {
+                        food.ingredients = productInfo.product.ingredients_text;
+                    } else {
+                        food.ingredients = "no ingredients listed";
+                    }
+
+                    // Produce HTML representation
+                    const foodAsHTML = foodFactory(food);
+
+                    // Add representaiton to DOM
+                    addFoodToDom(foodAsHTML);
+                });
         });
     });
+
+
+
+
+
 
 const foodFactory = (food) => {
 
